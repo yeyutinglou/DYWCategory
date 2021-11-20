7b06c63d2115848b1e85924b204a0db210ff4539
duyuwei <yeyutinglou@gmail.com>
2021-11-20 21:37:31 +0800

    gitignore

---
+++ Example/Pods/Target Support Files/DYWCategory/DYWCategory-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DYWCategory : NSObject
+@end
+@implementation PodsDummy_DYWCategory
+@end
---
+++ Example/Pods/Target Support Files/DYWCategory/DYWCategory-umbrella.h
@@ -0,0 +1,18 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "DYWCategory.h"
+#import "UIColor+DYWExtension.h"
+
+FOUNDATION_EXPORT double DYWCategoryVersionNumber;
+FOUNDATION_EXPORT const unsigned char DYWCategoryVersionString[];
+
